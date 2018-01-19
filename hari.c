73054a0f962449bc4e6072bec460b51c35118a92
haripreethi <31505883+haripreethi@users.noreply.github.com>
2018-01-19 16:36:24 +0530

    Create hari.c

---
+++ hari.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the year:",a);
+scanf("%d",&a);
+if(a%4==0)
+{
+printf("leap year",a)
+}
+else
+{
+printf("not leap year",a)
+}
+return 0;
+}
